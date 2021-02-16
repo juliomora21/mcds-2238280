e32a03e030ab533b7bfd953e382e9b1b79dc3070
juliomora21 <j.edwin.mora19@gmail.com>
2021-02-16 15:53:08 -0500

    actualizacion de la migracion de las tablas categories y games con un valor por defecto en foto, y se crean las semillas para sembrar datos en la tabla category

---
+++ larapp/database/seeders/CategorySeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use \App\Models\Category;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Creacion de semilla de Metodo Insert...
+        DB::table('categories')->insert([
+        	'name'        => 'Nintendo Switch',
+        	'description' => 'Lorem ipsun dolor sit amot',
+        ]);
+
+        // Creacion de semilla con metodo ORM
+        $cat = new Category;
+        $cat->name        = 'Xbox Series X';
+        $cat->description = 'Lorem ipsun dolor sit amot';
+        $cat->save();
+
+		$cat = new Category;
+        $cat->name        = 'Play Station 5';
+        $cat->description = 'Lorem ipsun dolor sit amot';
+        $cat->save();
+	}
+}
--- larapp/database/seeders/DatabaseSeeder.php
+++ larapp/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use \App\Models\User;
+use \App\Models\Category;
 
 
 class DatabaseSeeder extends Seeder
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
         	UserSeeder::class,
+        	CategorySeeder::class,
         ]);
     }
 }
